f967fce14a647b26ecdd68cc09b0b25ecdffd9c0
Ducaluz <duca.luz@gmail.com>
2024-04-11 17:03:29 -0300

    Obtendo um usuário por id

--- src/main/java/com/duca/workshopmongo/services/UserService.java
+++ src/main/java/com/duca/workshopmongo/services/UserService.java
@@ -1,12 +1,15 @@
 package com.duca.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.duca.workshopmongo.domain.User;
 import com.duca.workshopmongo.repository.UserRepository;
+import com.duca.workshopmongo.services.exception.ObjectNotFoundException;
+import com.mongodb.client.model.ReturnDocument;
 
 @Service
 public class UserService {
@@ -17,4 +20,9 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll(); 
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
