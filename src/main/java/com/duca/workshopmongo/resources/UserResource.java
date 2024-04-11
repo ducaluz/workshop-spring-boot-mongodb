fafc5a544340ac32ca7e8c18dbabcfe8c1dc7949
Ducaluz <duca.luz@gmail.com>
2024-04-11 19:50:39 -0300

    Atualização de usuário com PUT

--- src/main/java/com/duca/workshopmongo/resources/UserResource.java
+++ src/main/java/com/duca/workshopmongo/resources/UserResource.java
@@ -6,13 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.duca.workshopmongo.domain.User;
@@ -55,5 +49,15 @@ public class UserResource {
 		return ResponseEntity.noContent().build() ;
 				
 	}
-
+	
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		service.update(obj);
+		
+		return ResponseEntity.noContent().build() ;
+				
+	}
 }
