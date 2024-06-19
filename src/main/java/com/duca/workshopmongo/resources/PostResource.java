b957f01a4653b19cb45e746e4e17c238deacc302
Ducaluz <duca.luz@gmail.com>
2024-06-19 19:30:50 -0300

    Consulta com vários critérios

--- src/main/java/com/duca/workshopmongo/resources/PostResource.java
+++ src/main/java/com/duca/workshopmongo/resources/PostResource.java
@@ -1,6 +1,7 @@
 package com.duca.workshopmongo.resources;
 
 import java.net.URLDecoder;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,4 +38,19 @@ public class PostResource {
 				
 	}
 	
+
+	@GetMapping(value="/fullsearch")
+ 	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text",    defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		
+		return ResponseEntity.ok().body(list);
+	}
+
 }
